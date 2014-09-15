939a8bd942935221bfa29af74f71020f375dbcfd
Alejandro Covarrubias <acovarru@live.com>
2014-09-15 15:58:41 -0700

    updated css and html

--- P2.php
+++ P2.php
@@ -7,6 +7,8 @@
 <title>xkcd Password Generator P2</title> 
 <link rel="stylesheet" href="style.css">
   
+  <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   
 </head>
 
@@ -14,20 +16,28 @@
     xkcd Password Generator<br> 
 </h1>
 
-<body>
+<center>
     
-  
-  
     <form method="post">
-        
-        Word number <input type="text" name="word_number">
+        <br>
+        Word number <input type="text" name="word_number" size="1">
         <input type="submit" value="do it!"><br>
+        <br>
+       
+   
+
+   
+       
+        <input type="checkbox" name="number" value="number">Add number
+        <input type="checkbox" name="symbol" value="symbol">Add special symbol 
+       <input type="checkbox" name="upper" value="upper">Uppercase<br>
         
-        <input type="checkbox" name="number" value="number">Add number<br>
-        <input type="checkbox" name="symbol" value="symbol">Add special symbol<br> 
-        <input type="checkbox" name="upper" value="upper">Uppercase<br> 
-    </form> 
+  
+
     
+    </form> 
+ <div>   
+     <br>
 <?php
 include 'generate.php';
 //$generate= new generate();
@@ -105,12 +115,16 @@ else
 
 
 ?>
+</div>
 
+<body>
     <p style="color: black">What is xkcd password?</p>
 <font size="4" color="gray">XKCD, is a webcomic created by Randall Munroe   , this generator only uses common English words.
 That�s important, because the more unusual words are used, the harder the password will be to remember. </font>
 
 <p style="color: black">Description of your app </p>
 </body>
+
+</center>
 
 </html>
